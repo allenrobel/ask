a1e9f9a4919e9996941bef810c6fef65c24b9acd
Allen Robel <arobel@cisco.com>
2021-02-01 17:28:27 -1000

    20210201:allenrobel: initial commit

---
+++ unit_test/cisco/nxos/unit_test_nxos_bgp_neighbor.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# unit_test/cisco/nxos/unit_test_nxos_bgp_neighbor.py
+our_version = 101
+ 
+from ask.common.playbook import Playbook
+from ask.common.log import Log
+from ask.cisco.nxos.nxos_bgp_neighbor import NxosBgpNeighbor
+
+ansible_module = 'nxos_bgp_neighbor'
+ansible_host = 'dc-101' # must be in ansible inventory
+log = Log('unit_test_{}'.format(ansible_module), 'INFO', 'DEBUG')
+
+def playbook():
+    pb = Playbook(log)
+    pb.ansible_connection = 'httpapi' # httpapi, network_cli
+    pb.ansible_password = 'mypassword'
+    pb.file = '/tmp/playbook_{}.yaml'.format(ansible_module)
+    pb.name = '{} task'.format(ansible_module)
+    pb.add_host(ansible_host)
+    return pb
+
+def add_item_to_name(item, item_value, name):
+    value = ''
+    if item_value != None:
+        value = '{}, {} {}'.format(name, item, item_value)
+    else:
+        value = name
+    return value
+
+def add_task_name(task):
+    task_name = '{} {}'.format(ansible_module, ansible_host)
+    task_name = add_item_to_name('asn', task.asn, task_name)
+    task_name = add_item_to_name('neighbor', task.neighbor, task_name)
+    task_name = add_item_to_name('remote_as', task.remote_as, task_name)
+    task_name = add_item_to_name('state', task.state, task_name)
+    task_name = add_item_to_name('vrf', task.vrf, task_name)
+    task.task_name = task_name
+
+def add_task(pb):
+    task = NxosBgpNeighbor(log)
+    task.asn = '12000.0'
+    task.neighbor = '10.1.1.1'
+    task.remote_as = '6201.0'
+    task.state = 'present'
+    task.vrf = 'default'
+    task.task_name = add_task_name(task)
+    task.update()
+    pb.add_task(task)
+
+pb = playbook()
+add_task(pb)
+pb.append_playbook()
+pb.write_playbook()
+print('wrote {}'.format(pb.file))
