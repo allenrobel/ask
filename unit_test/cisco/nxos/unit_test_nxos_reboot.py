f42b21693faf6da2d4fb3f9e5053c47939156708
Allen Robel <arobel@cisco.com>
2021-02-05 10:12:15 -1000

    20210205:allenrobel: initial commit

---
+++ unit_test/cisco/nxos/unit_test_nxos_reboot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# unit_test/cisco/nxos/unit_test_nxos_reboot.py
+our_version = 103
+
+from ask.common.playbook import Playbook
+from ask.common.log import Log
+from ask.cisco.nxos.nxos_reboot import NxosReboot
+
+ansible_module = 'nxos_reboot'
+ansible_host = 'dc-101' # must be in ansible inventory
+log = Log('unit_test_{}'.format(ansible_module), 'INFO', 'DEBUG')
+
+def playbook():
+    pb = Playbook(log)
+    pb.ansible_connection = 'httpapi' # httpapi, network_cli
+    pb.ansible_password = 'mypassword'
+    pb.file = '/tmp/playbook_{}.yaml'.format(ansible_module)
+    pb.name = '{} task'.format(ansible_module)
+    pb.add_host(ansible_host)
+    return pb
+
+def add_item_to_name(item, item_value, name):
+    value = ''
+    if item_value != None:
+        value = '{}, {} {}'.format(name, item, item_value)
+    else:
+        value = name
+    return value
+
+def add_task_name(task):
+    task_name = '{} {}'.format(ansible_module, ansible_host)
+    task_name = add_item_to_name('confirm', task.confirm, task_name)
+    task.task_name = task_name
+
+def add_task(pb):
+    task = NxosReboot(log)
+    task.confirm = 'yes'
+    add_task_name(task)
+    task.update()
+    pb.add_task(task)
+
+pb = playbook()
+add_task(pb)
+pb.append_playbook()
+pb.write_playbook()
+log.info('wrote playbook {}'.format(pb.file))
