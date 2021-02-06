ecd42f320b6c911d1c12ea38e2b89b3754dca429
Allen Robel <arobel@cisco.com>
2021-02-05 19:55:47 -1000

    20210205:allenrobel: commited wrong version
    Replace last version with correct version

--- unit_test/ansible/unit_test_pause.py
+++ unit_test/ansible/unit_test_pause.py
@@ -1,47 +1,27 @@
 #!/usr/bin/env python3
 # unit_test/ansible/unit_test_pause.py
-our_version = 100
+our_version = 101
 '''
-Name: unit_test_pause.py
+==================
+unit_test_pause.py
+==================
 
-Description:
+Description
+-----------
 
 Generates Ansible playbook conformant with Ansible module: pause
-
-Prerequisites:
 '''
 from ask.common.playbook import Playbook
 from ask.common.log import Log
-from ask.cisco.nxos.nxos_evpn_global import NxosEvpnGlobal
+from ask.ansible.pause import Pause
 
 ansible_module = 'pause'
-ansible_host = 'dc-101' # must be in ansible inventory
+ansible_host = 'localhost'
 log = Log('unit_test_{}'.format(ansible_module), 'INFO', 'DEBUG')
 
 def playbook():
     pb = Playbook(log)
-    pb.ansible_connection = 'httpapi' # httpapi, network_cli
-    pb.ansible_password = 'mypassword'
-    pb.file = '/tmp/playbook_{}.yaml'.format(ansible_module)
-    pb.name = '{} task'.format(ansible_module)
-    pb.add_host(ansible_host)
-    return pb
-
-def pause_10():
-    task = AnsTaskPause(log)
-    task.seconds = 10
-    task.task_name = 'pause_10_seconds'
-    task.update()
-    return task
-def pause_20():
-    task = AnsTaskPause(log)
-    task.seconds = 20
-    task.task_name = 'pause_20_seconds'
-    task.update()
-    return task
-
-def pause_playbook_vars(pb):
-    pb.ansible_connection = None
+    pb.ansible_connection = 'local'
     pb.ansible_password = None
     pb.ansible_network_os = None
     pb.ansible_host_key_checking = None
@@ -50,11 +30,28 @@ def pause_playbook_vars(pb):
     pb.ansible_paramiko_pty = None
     pb.ansible_httpapi_validate_certs = None
     pb.ansible_httpapi_use_ssl = None
+    pb.file = '/tmp/playbook_{}.yaml'.format(ansible_module)
+    pb.name = '{}'.format(ansible_module)
+    pb.add_host(ansible_host)
+    return pb
+
+def pause_10(pb):
+    task = Pause(log)
+    task.seconds = 10
+    task.task_name = 'pause_{}_seconds'.format(task.seconds)
+    task.update()
+    pb.add_task(task)
+
+def pause_20(pb):
+    task = Pause(log)
+    task.seconds = 20
+    task.task_name = 'pause_{}_seconds'.format(task.seconds)
+    task.update()
+    pb.add_task(task)
 
 pb = playbook()
-pb.add_task(pause_10())
-pb.add_task(pause_20())
+pause_10(pb)
+pause_20(pb)
 pb.append_playbook()
-pause_playbook_vars(pb)
 pb.write_playbook()
 log.info('wrote {}'.format(pb.file))
