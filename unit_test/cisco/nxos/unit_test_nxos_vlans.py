c1550c53a0e7ddbdd8aa69ab1095b2c0d5d8373f
Allen Robel <arobel@cisco.com>
2021-02-06 16:41:11 -1000

    20210206:allenrobel: initial commit

---
+++ unit_test/cisco/nxos/unit_test_nxos_vlans.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# unit_test/cisco/nxos/unit_test_nxos_vlans.py
+our_version = 100
+
+from ask.common.playbook import Playbook
+from ask.common.log import Log
+from ask.cisco.nxos.nxos_vlans import NxosVlans
+
+ansible_module = 'nxos_vlans'
+ansible_host = 'dc-101' # must be in ansible inventory
+log = Log('unit_test_{}'.format(ansible_module), 'INFO', 'DEBUG')
+
+def playbook():
+    pb = Playbook(log)
+    pb.ansible_connection = 'httpapi' # httpapi, network_cli
+    pb.ansible_password = 'mypassword'
+    pb.file = '/tmp/playbook_{}.yaml'.format(ansible_module)
+    pb.name = '{} task'.format(ansible_module)
+    pb.add_host(ansible_host)
+    return pb
+
+def add_task(pb):
+    task = NxosVlans(log)
+    vlans = [10, 20]
+    vnis = [10010, 10020]
+
+    for vlan_id,mapped_vni in zip(vlans, vnis):
+        task.enabled = 'yes'
+        task.mapped_vni = mapped_vni
+        task.vlan_state = 'active'
+        task.vlan_id = vlan_id
+        task.name = 'vlan_{}_vni_{}'.format(task.vlan_id, task.mapped_vni)
+        task.add_vlan()
+    task.state = 'deleted'
+    task.task_name = '{}: vlans {}, vnis {} state {}'.format(ansible_module, vlans, vnis, task.state)
+    task.update()
+    pb.add_task(task)
+
+pb = playbook()
+add_task(pb)
+pb.append_playbook()
+pb.write_playbook()
+log.info('wrote {}'.format(pb.file))
