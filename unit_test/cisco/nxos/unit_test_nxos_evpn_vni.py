7138c1f249b6c6b6d6e38c77019a0be852394c28
Allen Robel <arobel@cisco.com>
2021-02-24 22:01:27 -1000

    20210224:allenrobel: reSTify documentation, more...
    Convert vni to str() to avoid Ansible warning.
    
    unit-test script, use True instead of 'yes'
    
    unit-test script, leverage task.append_to_name()

--- unit_test/cisco/nxos/unit_test_nxos_evpn_vni.py
+++ unit_test/cisco/nxos/unit_test_nxos_evpn_vni.py
@@ -20,30 +20,19 @@ def playbook():
     pb.add_host(ansible_host)
     return pb
 
+def add_task_name(task):
+    task.append_to_task_name('v.{}'.format(our_version))
+    task.append_to_task_name(ansible_host)
+    for key in sorted(task.properties_set):
+        task.append_to_task_name(key)
+
 def add_task_evpn_global(pb):
     ansible_module = 'nxos_evpn_global'
     task = NxosEvpnGlobal(log)
-    task.nv_overlay_evpn = 'yes'
-    task.task_name = '{} {}'.format(ansible_module, ansible_host)
+    task.nv_overlay_evpn = True
+    add_task_name(task)
     task.update()
     pb.add_task(task)
-
-def add_item_to_name(item, item_value, name):
-    value = ''
-    if item_value != None:
-        value = '{}, {} {}'.format(name, item, item_value)
-    else:
-        value = name
-    return value
-
-def add_task_name_nxos_evpn_vni(task):
-    task_name = '{} {}'.format(ansible_module, ansible_host)
-    task_name = add_item_to_name('route_distinguisher', task.route_distinguisher, task_name)
-    task_name = add_item_to_name('route_target_both', task.route_target_both, task_name)
-    task_name = add_item_to_name('route_target_import', task.route_target_import, task_name)
-    task_name = add_item_to_name('route_target_export', task.route_target_export, task_name)
-    task_name = add_item_to_name('state', task.state, task_name)
-    task.task_name = task_name
 
 def add_task_evpn_vni_rd_digits_format(pb):
     task = NxosEvpnVni(log)
@@ -52,7 +41,7 @@ def add_task_evpn_vni_rd_digits_format(pb):
     task.route_target_import = 'auto'
     task.route_target_export = 'auto'
     task.state = 'present'
-    add_task_name_nxos_evpn_vni(task)
+    add_task_name(task)
     task.update()
     pb.add_task(task)
 def add_task_evpn_vni_rd_ip_format(pb):
@@ -62,7 +51,7 @@ def add_task_evpn_vni_rd_ip_format(pb):
     task.route_target_import = ['1.2.3.4:304', '56220:1']
     task.route_target_export = ['65122:13']
     task.state = 'present'
-    add_task_name_nxos_evpn_vni(task)
+    add_task_name(task)
     task.update()
     pb.add_task(task)
 
