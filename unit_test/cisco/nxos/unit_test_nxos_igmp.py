fa869b0314e155af1369501c126d86127594c25f
Allen Robel <arobel@cisco.com>
2021-02-26 13:02:07 -1000

    20210226:allenrobel: reSTify documentation, more...
    Require bool() for all no/yes str() parameters
    
    unit_test_nxos_igmp.py - Update per above
    
    unit_test_nxos_igmp.py - leverage task.append_to_task_name()

--- unit_test/cisco/nxos/unit_test_nxos_igmp.py
+++ unit_test/cisco/nxos/unit_test_nxos_igmp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # unit_test/cisco/nxos/unit_test_nxos_igmp.py
-our_version = 101
+our_version = 102
 
 from ask.common.playbook import Playbook
 from ask.common.log import Log
@@ -19,27 +19,17 @@ def playbook():
     pb.add_host(ansible_host)
     return pb
 
-def add_item_to_name(item, item_value, name):
-    value = ''
-    if item_value != None:
-        value = '{}, {} {}'.format(name, item, item_value)
-    else:
-        value = name
-    return value
-
 def add_task_name(task):
-    task_name = '{} {}'.format(ansible_module, ansible_host)
-    task_name = add_item_to_name('enforce_rtr_alert', task.enforce_rtr_alert, task_name)
-    task_name = add_item_to_name('flush_routes', task.flush_routes, task_name)
-    task_name = add_item_to_name('restart', task.restart, task_name)
-    task_name = add_item_to_name('state', task.state, task_name)
-    task.task_name = task_name
+    task.append_to_task_name('v.{}'.format(our_version))
+    task.append_to_task_name(ansible_host)
+    for key in sorted(task.properties_set):
+        task.append_to_task_name(key)
 
 def add_task(pb):
     task = NxosIgmp(log)
-    task.enforce_rtr_alert = 'yes'
-    task.flush_routes = 'yes'
-    task.restart = 'yes'
+    task.enforce_rtr_alert = True
+    task.flush_routes = True
+    task.restart = True
     task.state = 'present'
     add_task_name(task)
     task.update()
