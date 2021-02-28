940970f388ab6301b2420658d3ad5ce033d26a31
Allen Robel <arobel@cisco.com>
2021-02-27 21:05:31 -1000

    20210227:allenrobel: reSTify documentation, more
    Accept only bool() for yes/no parameters
    Verify user input for seveeral parameters
    Update unit-test with several new tests
    Leverage task.append_to_task_name() in unit-test script

--- unit_test/cisco/nxos/unit_test_nxos_config.py
+++ unit_test/cisco/nxos/unit_test_nxos_config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # unit_test/cisco/nxos/unit_test_nxos_config.py
-our_version = 105
+our_version = 106
 
 from ask.common.playbook import Playbook
 from ask.common.log import Log
@@ -19,71 +19,101 @@ def playbook():
     pb.add_host(ansible_host)
     return pb
 
-def ntp():
+def add_task_name(task):
+    task.append_to_task_name('v.{}'.format(our_version))
+    task.append_to_task_name(ansible_host)
+    for key in sorted(task.properties_set):
+        task.append_to_task_name(key)
+
+def save_when(pb):
     task = NxosConfig(log)
-    task.task_name = '{} {} ntp'.format(ansible_host, ansible_module)
     cfg = list()
-    cfg.append('ntp server {} use-vrf management'.format('172.29.167.1'))
+    cfg.append('ntp server {} use-vrf management'.format('10.1.2.3'))
     cfg.append('ntp source-interface mgmt0')
     task.lines = cfg
+    task.save_when = 'modified'
+    add_task_name(task)
     task.update()
-    return task
+    pb.add_task(task)
 
-def hostname():
+def diff_against_startup(pb):
     task = NxosConfig(log)
-    task.task_name = '{} {} hostname'.format(ansible_host, ansible_module)
-    cfg = list()
-    cfg.append('hostname {}'.format(ansible_host))
-    task.lines = cfg
+    task.diff_against = 'startup'
+    add_task_name(task)
     task.update()
-    return task
+    pb.add_task(task)
 
-def bgp():
+def match(pb):
     task = NxosConfig(log)
-    task.task_name = '{} {} bgp_router'.format(ansible_host, ansible_module)
     cfg = list()
-    cfg.append('router bgp 17234')
-    cfg.append('  address-family ipv4 unicast')
-    cfg.append('    send-community')
-    cfg.append('  address-family ipv6 unicast')
-    cfg.append('    send-community')
-    cfg.append('  neighbor 1.1.1.1')
-    cfg.append('    inherit peer foo')
-    task.lines = cfg
-    task.update()
-    return task
+    cfg.append('10 permit ip 192.0.2.1/32 any log')
+    cfg.append('20 permit ip 192.0.2.2/32 any log')
+    cfg.append('30 permit ip 192.0.2.3/32 any log')
+    cfg.append('40 permit ip 192.0.2.4/32 any log')
+    cfg.append('50 permit ip 192.0.2.5/32 any log')
+    parents = list()
+    parents.append('ip access-list test')
+    before = list()
+    before.append('no ip access-list test')
 
-def tcam():
-    task = NxosConfig(log)
-    task.task_name = '{} {} tcam'.format(ansible_host, ansible_module)
-    cfg = list()
-    cfg.append('hardware access-list tcam region ipv6-ifacl 0')
-    cfg.append('hardware access-list tcam region mac-ifacl 0')
-    cfg.append('hardware access-list tcam region ipv6-racl 0')
-    cfg.append('hardware access-list tcam region e-racl 4096')
     task.lines = cfg
+    task.parents = parents
+    task.before = before
+    task.match = 'exact'
+    add_task_name(task)
     task.update()
-    return task
+    pb.add_task(task)
 
-def vrf_context_management():
+def replace_block(pb):
     task = NxosConfig(log)
-    task.task_name = '{} {} vrf context management'.format(ansible_host, ansible_module)
     cfg = list()
-    cfg.append('vrf context management')
-    cfg.append('ip name-server {}'.format('172.29.167.250'))
-    cfg.append('ip route 0.0.0.0/0 {}'.format('172.29.167.1'))
-    cfg.append('ipv6 route ::/0 2001:420:283:2000:3:11:160:1')
+    cfg.append('10 permit ip 192.0.2.1/32 any log')
+    cfg.append('20 permit ip 192.0.2.2/32 any log')
+    cfg.append('30 permit ip 192.0.2.3/32 any log')
+    cfg.append('40 permit ip 192.0.2.4/32 any log')
+    parents = list()
+    parents.append('ip access-list test')
+    before = list()
+    before.append('no ip access-list test')
+
     task.lines = cfg
+    task.parents = parents
+    task.before = before
+    task.replace = 'block'
+    add_task_name(task)
     task.update()
-    return task
+    pb.add_task(task)
+
+def replace_config(pb):
+    task = NxosConfig(log)
+    task.replace_src = 'bootflash:/config.txt'
+    task.replace = 'config'
+    add_task_name(task)
+    task.update()
+    pb.add_task(task)
+
+def parents(pb):
+    task = NxosConfig(log)
+    cfg = list()
+    cfg.append('shutdown')
+    parents = list()
+    parents.append('router bgp 64518')
+    parents.append('neighbor 10.0.1.2')
+    task.lines = cfg
+    task.parents = parents
+
+    add_task_name(task)
+    task.update()
+    pb.add_task(task)
 
 pb = playbook()
 
-pb.add_task(hostname())
-pb.add_task(ntp())
-pb.add_task(vrf_context_management())
-pb.add_task(tcam())
-pb.add_task(bgp())
+#save_when(pb)
+diff_against_startup(pb)
+#match(pb)
+#replace_block(pb)
+#replace_config(pb)
+#parents(pb)
 
 pb.append_playbook()
 pb.write_playbook()
