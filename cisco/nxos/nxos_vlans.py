ecf004e85632998d383802a336eee5e64f16a71b
Allen Robel <arobel@cisco.com>
2021-03-29 15:49:00 -1000

    20210329:allenrobel: commit(), new alias for update(), more...
    commit() sounds more descriptive than update().  We will modify all libraries to use commit(), but leave update() as an alias for backwward compatibility
    
    Also:
    
    1. Add documentation for user methods
    2. Example script: Update to use commit()
    3. Example script: Add comment to add_task_name()

--- cisco/nxos/nxos_vlans.py
+++ cisco/nxos/nxos_vlans.py
@@ -1,5 +1,5 @@
 # NxosVlans() - cisco/nxos/nxos_vlans.py
-our_version = 107
+our_version = 108
 from copy import deepcopy
 from ask.common.task import Task
 '''
@@ -10,6 +10,10 @@ NxosVlans()
 .. contents::
    :local:
    :depth: 1
+
+Version
+-------
+108
 
 ScriptKit Synopsis
 ------------------
@@ -24,6 +28,55 @@ ScriptKit Example
 -----------------
 - `unit_test/cisco/nxos/unit_test_nxos_vlans.py <https://github.com/allenrobel/ask/blob/main/unit_test/cisco/nxos/unit_test_nxos_vlans.py>`_
 
+|
+
+========================    ============================================
+Method                      Description
+========================    ============================================
+add_vlan()                  Perform validation on the currently-set vlan
+                            properties, and commit to the vlan list::
+
+                                - Type: function()
+                                - Example:
+                                    # For full script, see ScriptKit Example above
+                                    task = NxosVlans(log)
+                                    task.append_to_task_name('Enable vlans')
+                                    vlans = [10, 20]
+                                    vnis = [10010, 10020]
+                                    for vlan_id,mapped_vni in zip(vlans, vnis):
+                                        task.enabled = True
+                                        task.mapped_vni = mapped_vni
+                                        task.vlan_state = 'active'
+                                        task.vlan_id = vlan_id
+                                        task.append_to_task_name(task.vlan_id)
+                                        task.add_vlan()
+                                    task.state = 'merged'
+                                    task.commit()
+                                    pb.add_task(task)
+
+                                - Resulting playbook task:
+
+                                    tasks:
+                                    -   cisco.nxos.nxos_vlans:
+                                            config:
+                                            -   enabled: true
+                                                mapped_vni: 10010
+                                                state: active
+                                                vlan_id: 10
+                                            -   enabled: true
+                                                mapped_vni: 10020
+                                                state: active
+                                                vlan_id: 20
+                                            state: merged
+                                        name: '[cisco.nxos.nxos_vlans : v.109], Enable vlans, 10, 20'
+
+commit()                    Perform final verification and commit the 
+                            current task::
+                                - Type: function()
+                                - Alias: update()
+                                - Example - see add_vlan()
+
+========================    ============================================
 
 |
 
@@ -172,6 +225,8 @@ class NxosVlans(Task):
             self.task_log.error('exiting. call instance.state before calling instance.update()')
             exit(1)
 
+    def commit(self):
+        self.update()
     def update(self):
         '''
         call final_verification()
--- unit_test/cisco/nxos/unit_test_nxos_vlans.py
+++ unit_test/cisco/nxos/unit_test_nxos_vlans.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # unit_test/cisco/nxos/unit_test_nxos_vlans.py
-our_version = 103
+our_version = 104
 
 from ask.common.playbook import Playbook
 from ask.common.log import Log
@@ -20,25 +20,37 @@ def playbook():
     return pb
 
 def add_task_name(task):
+    '''
+    add_task_name() can be used to add all currently-set properties
+    and their values to the task name.  If a user-accessible property
+    key is passed to task.append_to_task_name(), the key AND its
+    value are appended to the task name.  All ScriptKit libraries
+    include the property task.scriptkit_properties, which is a
+    python set() containing all user-accessible property keys
+    supported by the library.
+
+    We are not using add_task_name() in this script, but leave
+    it here as an example should you want to leverage it.
+    '''
     task.append_to_task_name('v{}, {}'.format(our_version, ansible_host))
     for key in sorted(task.scriptkit_properties):
         task.append_to_task_name(key)
 
 def add_task(pb):
     task = NxosVlans(log)
+    task.append_to_task_name('Enable vlans')
     vlans = [10, 20]
     vnis = [10010, 10020]
 
     for vlan_id,mapped_vni in zip(vlans, vnis):
-        task.enabled = True
+        task.enabled = Trues val
         task.mapped_vni = mapped_vni
         task.vlan_state = 'active'
         task.vlan_id = vlan_id
-        add_task_name(task)
+        task.append_to_task_name(task.vlan_id)
         task.add_vlan()
-    task.state = 'deleted'
-    task.task_name = '{}: vlans {}, vnis {} state {}'.format(ansible_module, vlans, vnis, task.state)
-    task.update()
+    task.state = 'merged'
+    task.commit()
     pb.add_task(task)
 
 pb = playbook()
