5963cee9b6a380b723fc03651e5345834050fb16
Allen Robel <arobel@cisco.com>
2021-08-01 10:43:39 -1000

    20210801:allenrobel:exit(1) on errors, more...
    Fix import status in Synopsis
    Print expected values on invalid input
    General cleanup

--- common/log.py
+++ common/log.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+our_version = 109
 '''
 ==============
 Log() - log.py
@@ -18,13 +19,12 @@ log = Log('my_logger_name', INFO', 'DEBUG')
 
 Notes
 -----
-1. Valid logging levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
+1. Valid logging levels: CRITICAL, DEBUG, ERROR, INFO, WARNING
 '''
 
 import os
 import logging
 import logging.handlers
-our_version = 108
 
 def Log(_name, _console_level='INFO', _file_level='DEBUG', _capture_warnings=True):
     '''
@@ -45,7 +45,7 @@ class Logger(object):
 
     ::
 
-    from log import Logger
+    from ask.common.log import Logger
 
     logger = Logger()
     logger.logfile = '/tmp/foobar.log'
@@ -56,17 +56,24 @@ class Logger(object):
     log.error('this is an error log')
     '''
     def __init__(self):
-        self._loglevel = 'INFO'
-        self._logfile = '/tmp/logger.log'
-        self._levels = dict()
-        self._levels['DEBUG'] = logging.DEBUG
-        self._levels['INFO'] = logging.INFO
-        self._levels['WARNING'] = logging.WARNING
-        self._levels['ERROR'] = logging.ERROR
-        self._levels['CRITICAL'] = logging.CRITICAL
+        self.properties = dict()
+        self.properties['logfile'] = '/tmp/logger.log'
+        self.properties['file_loglevel'] = 'DEBUG'
+        self.properties['console_loglevel'] = 'ERROR'
 
-        self._file_loglevel = self._levels['DEBUG']
-        self._console_loglevel = self._levels['ERROR']
+        self.valid_loglevels = set()
+        self.valid_loglevels.add('DEBUG')
+        self.valid_loglevels.add('INFO')
+        self.valid_loglevels.add('WARNING')
+        self.valid_loglevels.add('ERROR')
+        self.valid_loglevels.add('CRITICAL')
+
+        self.loglevel_map = dict()
+        self.loglevel_map['DEBUG'] = logging.DEBUG
+        self.loglevel_map['INFO'] = logging.INFO
+        self.loglevel_map['WARNING'] = logging.WARNING
+        self.loglevel_map['ERROR'] = logging.ERROR
+        self.loglevel_map['CRITICAL'] = logging.CRITICAL
 
         self.log = None
 
@@ -92,39 +99,43 @@ class Logger(object):
 
     @property
     def file_loglevel(self):
-        return self._file_loglevel
+        return self.properties['file_loglevel']
     @file_loglevel.setter
     def file_loglevel(self, _x):
-        if _x.upper() not in self._levels:
-            return
+        if _x.upper() not in self.valid_loglevels:
+            print('exiting. Logger().file_loglevel invalid: {}. Expected one of {}'.format(
+                _x,
+                sorted(self.valid_loglevels)))
+            exit(1)
         if self.log == None:
-            print("Ignored.  call instance.new() first.")
-            return
-        self._file_loglevel = self._levels[_x.upper()]
-        self.fh.setLevel(self._file_loglevel)
+            print("exiting. Logger().file_loglevel. Call Logger().new() first.")
+            exit(1)
+        self.properties['file_loglevel'] = self.loglevel_map[_x.upper()]
+        self.fh.setLevel(self.properties['file_loglevel'])
         self.log.debug('set file_loglevel to {}'.format(_x))
 
     @property
     def console_loglevel(self):
-        return self._console_loglevel
+        return self.properties['console_loglevel']
     @console_loglevel.setter
     def console_loglevel(self, _x):
-        if _x.upper() not in self._levels:
-            print('unknown loglevel {}'.format(_x))
-            return
+        if _x.upper() not in self.valid_loglevels:
+            print('exiting. Logger().console_loglevel invalid: {}. Expected one of {}'.format(
+                _x,
+                sorted(self.valid_loglevels)))
+            exit(1)
         if self.log == None:
-            print("Ignored.  call instance.new() first.")
-            return
-        self._console_loglevel = self._levels[_x.upper()]
-        self.ch.setLevel(self._console_loglevel)
+            print("exiting. Logger().console_loglevel. Call Logger().new() first.")
+            exit(1)
+        self.properties['console_loglevel'] = self.loglevel_map[_x.upper()]
+        self.ch.setLevel(self.properties['console_loglevel'])
         self.log.debug('set console_loglevel to {}'.format(_x))
 
     @property
     def logfile(self):
-        return self._logfile
+        return self.properties['logfile']
     @logfile.setter
     def logfile(self, _x):
         if self.log != None:
-            print("Ignoring. instance.new() was already called.  Call instance.logfile prior to calling instance.new()")
-        self._logfile = _x
-
+            print("Ignoring. Logger().logfile Logger().new() was already called.  Call Logger().logfile() prior to calling Logger().new()")
+        self.properties['logfile'] = _x
